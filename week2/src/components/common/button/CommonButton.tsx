fa8a0171af06b3363a2f20c17a327e2470d69931
selim-jo <skyblue.777@daum.net>
2024-11-13 23:26:37 +0900

    [add] week2 공통 푸터 컴포넌트 작업

--- week2/src/components/common/button/CommonButton.tsx
+++ week2/src/components/common/button/CommonButton.tsx
@@ -5,11 +5,13 @@ const CommonButton = ({
     size = 'medium', // 버튼 사이즈 (small / medium / large)
     variant = 'filled', // 버튼 스타일 (filled / outlined)
     icon,
+    style = {},
     onClick // 클릭 이벤트
 }) => {
   return (
     <button 
         className={`${styles.button} ${styles[size]} ${styles[variant]} ${styles.withIcon}`} 
+        style={style}
         onClick={onClick}
     >
         {icon && <img src={icon} alt="icon" className={styles.icon} />}
--- week2/src/pages/index/index.tsx
+++ week2/src/pages/index/index.tsx
@@ -1,10 +1,14 @@
+import CommonFooter from '@/components/common/footer/CommonFooter';
 import CommonHeader from '../../components/common/header/CommonHeader';
 import styles from './styles/index.module.scss';
 
 const index = () => {
   return (
     <div className={styles.page}>
+      {/* 공통 헤더 */}
       <CommonHeader />
+      {/* 공통 푸터 */}
+      <CommonFooter />
     </div>
   );
 }
