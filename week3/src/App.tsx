e22bdce864611e0991ca9ca6850147ad858d89e0
selim-jo <skyblue.777@daum.net>
2024-11-18 22:02:12 +0900

    [add] week3 react-responsive 라이브러리 사용해보기

--- week3/src/App.tsx
+++ week3/src/App.tsx
@@ -1,9 +1,36 @@
+// react-responsive 라이브러리 사용하여 반응형 웹 구현
+import { useMediaQuery } from 'react-responsive';
+import type { PropsWithChildren } from 'react';
+
+const Desktop = ({ children }: PropsWithChildren) => {
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
+  return isDesktop ? children : null;
+};
+
+const Tablet = ({ children }: PropsWithChildren) => {
+  const isTablet = useMediaQuery({ minWidth: 834, maxWidth: 1279 });
+  return isTablet ? children : null;
+};
+
+const Mobile = ({ children }: PropsWithChildren) => {
+  const isMobile = useMediaQuery({ maxWidth: 833 });
+  return isMobile ? children : null;
+};
+
 function App() {
 
   return (
-    <>
-      <div>테스트</div>
-    </>
+    <div>
+      <Desktop>
+        <div>데스크탑</div>
+      </Desktop>
+      <Tablet>
+        <div>태블릿</div>
+      </Tablet>
+      <Mobile>
+        <div>모바일</div>
+      </Mobile>
+    </div>
   );
 }
 
